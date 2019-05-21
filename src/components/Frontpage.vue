0135fdcf6d0372adb8d0f711d2be7427f512e1ea
Halldor Valur Stefansson <doristef@gmail.com>
2019-05-21 21:43:05 +0200

    Added icons, styling css for frontpage

--- src/components/Frontpage.vue
+++ src/components/Frontpage.vue
@@ -1,30 +1,42 @@
 <template>
   <b-container fluid>
     <!--- Holidaze --->
-    <b-row class="[ justify-content-center ][ my-5 ]" align-v="center">
-      <b-col align-self="center">
+    <b-row
+      class="[ justify-content-center ][ my-5 ]"
+      align-v="center"
+      align-h="around"
+    >
+      <b-col align-self="center" col cols="12" lg="5">
         <b-container>
           <h1 class="[ frontpage-h1 ]">Holidaze</h1>
 
           <p>
-            A local tourist agency located in Bergen.
+            A local tourism agency located in Bergen, aiming to help visitors of
+            Norway to find great accommodation.
           </p>
         </b-container>
       </b-col>
-    </b-row>
-    <!--- Holidaze END --->
-    <!--- Icons --->
-    <b-row
-      class="[ justify-content-center ][ frontpage-icons ]"
-      align-v="center"
-    >
-      <b-col align-self="center">
-        <b-container>
-          <span class="[ icon-suitcase ]"> suitcase </span>
-          <span class="[ icon-money ]"> money </span>
 
-          <p>Something about suitcase</p>
-          <p>Something about money</p>
+      <b-col
+        align-self="center"
+        col
+        cols="12"
+        lg="3"
+        class="[ icon-container ]"
+      >
+        <b-container>
+          <b-row align-v="center">
+            <span class="[ icon icon-suitcase ]" />
+            <p class="[ ml-5 ][ icon-text ]">
+              Plan your vacation in the comfort of your own home.
+            </p>
+          </b-row>
+          <b-row align-v="center">
+            <span class="[ icon icon-money ]" />
+            <p class="[ ml-5 ][ icon-text ]">
+              Save money, Find the best accommodation for reasonable price.
+            </p>
+          </b-row>
         </b-container>
       </b-col>
     </b-row>
