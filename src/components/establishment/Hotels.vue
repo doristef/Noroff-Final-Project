75e48f3ecf8f3fbd21b521d099b71c84696ca9ae
Halldor Valur Stefansson <doristef@gmail.com>
2019-06-10 17:45:49 +0200

    Added sorting to All Establishments/Hotels

--- src/components/establishment/Hotels.vue
+++ src/components/establishment/Hotels.vue
@@ -1,11 +1,75 @@
 <template>
   <b-container class="[ mt-2 ]">
     <b-row align-h="center">
+      <b-col align-self="center" class="[ my-1 ][ text-center ]">
+        <b-dropdown text="Sort Establishments By : " variant="secondary">
+          <b-dropdown-item @click="changeSortKey('establishmentName')">
+            <a href="#" id="establishmentName"
+              >Name
+              <font-awesome-icon
+                icon="check"
+                size="1x"
+                class="[ mr-2 ]"
+                v-if="sortKey === 'establishmentName'"
+              />
+            </a>
+          </b-dropdown-item>
+          <b-dropdown-item @click="changeSortKey('price')">
+            <a href="#" id="price"
+              >Price
+              <font-awesome-icon
+                icon="check"
+                size="1x"
+                class="[ mr-2 ]"
+                v-if="sortKey === 'price'"
+              />
+            </a>
+          </b-dropdown-item>
+          <b-dropdown-item @click="changeSortKey('maxGuests')">
+            <a href="#" id="maxGuests"
+              >Max. Guests
+              <font-awesome-icon
+                icon="check"
+                size="1x"
+                class="[ mr-2 ]"
+                v-if="sortKey === 'maxGuests'"
+              />
+            </a>
+          </b-dropdown-item>
+
+          <b-dropdown-divider></b-dropdown-divider>
+
+          <b-dropdown-item @click="changeSortDir('asc')">
+            <a href="#" id="asc"
+              >Asc
+              <font-awesome-icon
+                icon="check"
+                size="1x"
+                class="[ mr-2 ]"
+                v-if="sortDir === 'asc'"
+              />
+            </a>
+          </b-dropdown-item>
+          <b-dropdown-item @click="changeSortDir('desc')">
+            <a href="#" id="desc"
+              >Desc
+              <font-awesome-icon
+                icon="check"
+                size="1x"
+                class="[ mr-2 ]"
+                v-if="sortDir === 'desc'"
+              />
+            </a>
+          </b-dropdown-item>
+        </b-dropdown>
+      </b-col>
+    </b-row>
+    <b-row align-h="center">
       <!--- CARDS --->
       <b-card
         no-body
         bg-variant="transparent"
-        v-for="establishment in establishments"
+        v-for="establishment in sortBy"
         :key="establishment.id"
         tag="article"
         class="[ card-hotel ][ mx-2 my-4 ]"
@@ -62,6 +126,39 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 export default {
   name: "establishments",
   components: { FontAwesomeIcon },
-  props: ["establishments"]
+  props: ["establishments"],
+  data() {
+    return {
+      sortKey: "establishmentName",
+      sortDir: "asc"
+    };
+  },
+  computed: {
+    // SortBy for Enquiries
+    sortBy: function() {
+      // eslint-disable-next-line
+        return _.orderBy(
+        this.establishments,
+        function(item) {
+          if (this.sortKey === "price" || this.sortKey === "maxGuests") {
+            return parseInt(item[this.sortKey]);
+          } else {
+            return item[this.sortKey].toLowerCase();
+          }
+        }.bind(this),
+        this.sortDir
+      );
+    }
+  },
+  methods: {
+    // Change Sort Direction
+    changeSortDir: function(dir) {
+      return (this.sortDir = dir);
+    },
+    // Change Sort Key
+    changeSortKey: function(key) {
+      return (this.sortKey = key);
+    }
+  }
 };
 </script>
