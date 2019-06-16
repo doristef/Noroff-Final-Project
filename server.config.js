7aac47f0440a329c85806314a4d7be94b209152f
Halldor Valur Stefansson <doristef@gmail.com>
2019-06-16 20:27:43 +0200

    Cleared, ready for users URL

--- server.config.js
+++ server.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // URL to JSON - PHP file folder
-  URL: "http://doristef.me/semester4/FinalProject/server/",
+  URL: "", // URL to the /server/ folder, that contains JSON and PHP files!
   // Needed for Unblocking Cross-Origin request
   corsURL:
     process.env.NODE_ENV === "production"
