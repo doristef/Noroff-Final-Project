adae7c7da140d649c206bf8b40947e1a531fe24b
Halldór Valur Stefánsson <doristef@halldors-mbp.lan>
2021-05-08 00:29:28 +0200

    Updates for 2021

--- server.config.js
+++ server.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // URL to JSON - PHP file folder
-  URL: "", // URL to the /server/ folder, that contains JSON and PHP files!
+  URL: "http://doristef.me/semester4/FinalProject/server/", // URL to the /server/ folder, that contains JSON and PHP files!
   // Needed for Unblocking Cross-Origin request
   corsURL:
     process.env.NODE_ENV === "production"
